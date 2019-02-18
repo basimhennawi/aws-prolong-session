b7a11a774b1d3047275454504b07d07cf6c74247
Basim Hennawi <basim.a.hennawi@gmail.com>
2019-02-18 19:14:14 +0100

    Run every 11 hours Mon-Fri

--- scripts/run.js
+++ scripts/run.js
@@ -5,7 +5,7 @@ let count = 0;
 
 module.exports = () => {
     prolong(); // Run once in the beginning
-    new CronJob('0 */11 * * *', function() { // Run every 11 hours
+    new CronJob('0 */11 * * 1-5', function() { // Run every 11 hours Mon-Fri
         console.log('Job runs now for ' + ++count + ' time(s)');
         prolong();
     }, null, true, 'America/Los_Angeles');
