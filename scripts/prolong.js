37f0a8a60cb1b3ca9ab1eb11d7252bd6be4e9ecf
Basim Hennawi <basim.a.hennawi@gmail.com>
2019-04-05 17:32:01 +0200

    Every one hour reload

--- scripts/prolong.js
+++ scripts/prolong.js
@@ -18,89 +18,87 @@ const arrowSelector = '#gwt-debug-toggleButton';
 const dropdownMenuSelector = '#dashboard > div.cwdb-dashboard.cwdb-page > div.cwdb-toolbar > div.cwdb-refresh-controls.btn-group > div > div > span';
 const autoRefreshSelector = '#dashboard > div.cwdb-dashboard.cwdb-page > div.cwdb-toolbar > div.cwdb-refresh-controls.btn-group > div.cwui-dropdown > ul > li:nth-child(1) > label > span';
 
-const page = null;
+exports.open = async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        executablePath: '/usr/bin/chromium-browser',
+        args: ['--start-fullscreen']
+    });
 
-exports.open = () => {
-    (async () => {
-        const browser = await puppeteer.launch({
-            headless: false,
-            executablePath: '/usr/bin/chromium-browser',
-            args: ['--start-fullscreen']
-        });
+    page = await browser.newPage();
+    await page.setViewport({ width: 1920, height: 1080 });
+    await page.goto(awsConsoleUrl);
 
-        page = await browser.newPage();
-        await page.setViewport({ width: 1920, height: 1080 });
-        await page.goto(awsConsoleUrl);
+    // Wait for account ID input
+    await page.waitForSelector(accountIdSelector);
 
-        // Wait for account ID input
-        await page.waitForSelector(accountIdSelector);
+    // Set account ID input
+    await page.click(accountIdSelector);
+    await page.type(accountId);
 
-        // Set account ID input
-        await page.click(accountIdSelector);
-        await page.type(accountId);
+    // Navigate from FIRST to LOGIN
+    await Promise.all([
+        page.waitForNavigation({ timeout: 60000 }),
+        page.click(nextSelector),
+    ]);
 
-        // Navigate from FIRST to LOGIN
-        await Promise.all([
-            page.waitForNavigation({ timeout: 60000 }),
-            page.click(nextSelector),
-        ]);
+    // Wait for login inputs
+    await Promise.all([
+        page.waitForSelector(usernameSelector),
+        page.waitForSelector(passwordSelector),
+    ]);
 
-        // Wait for login inputs
-        await Promise.all([
-            page.waitForSelector(usernameSelector),
-            page.waitForSelector(passwordSelector),
-        ]);
+    // Set login input
+    await page.click(usernameSelector);
+    await page.type(username);
+    await page.click(passwordSelector);
+    await page.type(password);
 
-        // Set login input
-        await page.click(usernameSelector);
-        await page.type(username);
-        await page.click(passwordSelector);
-        await page.type(password);
+    // Navigate from LOGIN to HOME
+    await Promise.all([
+        page.waitForNavigation({ timeout: 60000 }),
+        page.click(siginSelector),
+    ]);
 
-        // Navigate from LOGIN to HOME
-        await Promise.all([
-            page.waitForNavigation({ timeout: 60000 }),
-            page.click(siginSelector),
-        ]);
+    await page.waitForSelector(servicesTabSelector);
+    
+    // Navigate directly to MainDashboard
+    await page.goto(dashboardUrl);
 
-        await page.waitForSelector(servicesTabSelector);
-        
-        // Navigate directly to MainDashboard
-        await page.goto(dashboardUrl);
-
-        prolong();
-    })();
+    return page;
 };
 
-exports.prolong = () => {
-    (async () => {
-        try {
-            await page.reload();
-            console.log('Prolonged successfully for another 12 hours!');
-        } catch (e) {
-            console.log("Prolonging the session by reloading the page failed.")
-        }
-        try {
-            await page.waitForSelector(arrowSelector);
-            await page.click(arrowSelector);
-        } catch (e) {
-            console.log("Left arrow element from the side menu not found.")
-        }
-        try {
-            await page.waitForSelector(dropdownMenuSelector);
-            await page.click(dropdownMenuSelector);
-            await page.waitForSelector(autoRefreshSelector);
-            await page.click(autoRefreshSelector);
-            await page.waitFor(1000);
-            await page.click(dropdownMenuSelector);
-        } catch (e) {
-            console.log("Dropdown menu from the top right menu not found.")
-        }
-        try {
-            await page.waitForSelector(twelveHoursRangeSelector);
-            await page.click(twelveHoursRangeSelector);
-        } catch (e) {
-            console.log("12h range element not found.")
-        }
-    })();
+let page;
+exports.prolong = async (_page) => {
+    // Set page globally on first time (ugly i know)
+    if (_page) page = _page;
+
+    try {
+        await page.reload();
+        console.log('Prolonged successfully for another hour!');
+    } catch (e) {
+        console.log("Prolonging the session by reloading the page failed.")
+    }
+    try {
+        await page.waitForSelector(arrowSelector);
+        await page.click(arrowSelector);
+    } catch (e) {
+        console.log("Left arrow element from the side menu not found.")
+    }
+    try {
+        await page.waitForSelector(dropdownMenuSelector);
+        await page.click(dropdownMenuSelector);
+        await page.waitForSelector(autoRefreshSelector);
+        await page.click(autoRefreshSelector);
+        await page.waitFor(1000);
+        await page.click(dropdownMenuSelector);
+    } catch (e) {
+        console.log("Dropdown menu from the top right menu not found.")
+    }
+    try {
+        await page.waitForSelector(twelveHoursRangeSelector);
+        await page.click(twelveHoursRangeSelector);
+    } catch (e) {
+        console.log("12h range element not found.")
+    }
 };
--- scripts/run.js
+++ scripts/run.js
@@ -1,14 +1,7 @@
 const CronJob = require('cron').CronJob;
 const {open, prolong} = require('./prolong');
 
-let count = 0;
-
 module.exports = () => {
-    open(); // Run once in the beginning
-    // new CronJob('0 */11 * * 1-5', function() { // Run every 11 hours Mon-Fri
-    // '0 0 * ? * MON,TUE,WED,THU,FRI *' // At second :00 of minute :00 of every hour, on every Monday, Tuesday, Wednesday, Thursday and Friday, every month
-    new CronJob('0/30 * * ? * 2-6 *', function() { // Run every 10 seconds
-        console.log('Job runs now for ' + ++count + ' time(s)');
-        prolong();
-    }, null, true, 'America/Los_Angeles');
+    open().then(prolong); // Run once in the beginning
+    new CronJob('0 * * * *', prolong, null, true, 'America/Los_Angeles');
 };
