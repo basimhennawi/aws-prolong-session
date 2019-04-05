0fd3e7196bfa192779d1836593f673055db59446
Basim Hennawi <basim.a.hennawi@gmail.com>
2019-04-05 16:38:13 +0200

    WIP #1

--- scripts/run.js
+++ scripts/run.js
@@ -7,7 +7,7 @@ module.exports = () => {
     open(); // Run once in the beginning
     // new CronJob('0 */11 * * 1-5', function() { // Run every 11 hours Mon-Fri
     // '0 0 * ? * MON,TUE,WED,THU,FRI *' // At second :00 of minute :00 of every hour, on every Monday, Tuesday, Wednesday, Thursday and Friday, every month
-    new CronJob('0/30 * * ? * MON,TUE,WED,THU,FRI *', function() { // Run every 10 seconds
+    new CronJob('0/30 * * ? * 2-6 *', function() { // Run every 10 seconds
         console.log('Job runs now for ' + ++count + ' time(s)');
         prolong();
     }, null, true, 'America/Los_Angeles');
